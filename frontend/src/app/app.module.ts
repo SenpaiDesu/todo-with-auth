50e2e4037f67c690c9fe88d5906243b8838297a5
SenpaiDesu <oregusenpai@gmail.com>
2017-10-08 02:27:29 -0700

    done

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-//import { StoreModule } from '@ngrx/store';
 import { 
   MatCardModule,
   MatInputModule,
